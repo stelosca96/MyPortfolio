cb662b63d75a59694e94d1c881b71432c6be8959
Stefano Loscalzo <stefano.loscalzo@gmail.com>
2021-03-23 22:07:51 +0100

    Gtag

--- rollup.config.js
+++ rollup.config.js
@@ -24,6 +24,7 @@ export default {
 		output: config.client.output(),
 		plugins: [
 			replace({
+				preventAssignment: true,
 				'process.browser': true,
 				'process.env.NODE_ENV': JSON.stringify(mode)
 			}),
@@ -71,6 +72,7 @@ export default {
 		output: config.server.output(),
 		plugins: [
 			replace({
+				preventAssignment: true,
 				'process.browser': false,
 				'process.env.NODE_ENV': JSON.stringify(mode)
 			}),
@@ -98,6 +100,7 @@ export default {
 		plugins: [
 			resolve(),
 			replace({
+				preventAssignment: true,
 				'process.browser': true,
 				'process.env.NODE_ENV': JSON.stringify(mode)
 			}),
